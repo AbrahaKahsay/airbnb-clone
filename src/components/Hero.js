b6a85f6db8dd0d54768fcf3f01219078e9f6fba8
AbrahaKahsay <abrahak81@gmail.com>
2023-01-05 16:45:44 +0300

    Modify hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import Grid from "../images/Group-grid.png"
+import Grid from "../images/Group-grid2.png"
 
 export const Hero = () => {
   return (
-    <div className='grid'>
-        <img src={Grid} alt='grid'/>
+    <div>
+        <div className='grid'>
+            <img src={Grid} alt='grid'/>
+        </div>
+        <h1>Online Experiences</h1>
+        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
     </div>
+   
   )
 }
