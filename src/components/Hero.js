0d5f70a4fc35ce937762d140791a2bab2da78548
AbrahaKahsay <abrahak81@gmail.com>
2023-01-05 14:35:03 +0300

    Add image-grid to hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Grid from "../images/Group-grid.png"
+
+export const Hero = () => {
+  return (
+    <div className='grid'>
+        <img src={Grid} alt='grid'/>
+    </div>
+  )
+}
