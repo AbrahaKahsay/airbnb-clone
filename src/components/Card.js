7f959f64a5fbcdd2042a094e520f66786b6f6c20
AbrahaKahsay <abrahak81@gmail.com>
2023-01-09 22:21:43 +0300

    Import card images to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import Image1 from "../images/image1.png"
+import Star1 from "../images/star1.png"
+import Elipse from "../images/Ellipse6.png"
+
 export const Card = () => {
   return (
     <div>
-        
+
     </div>
   )
 }
