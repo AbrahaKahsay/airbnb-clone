e50d555f74a49dcb747b53bd56dfb8fe494ae460
AbrahaKahsay <abrahak81@gmail.com>
2023-01-09 23:08:19 +0300

    Add card sibling to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from './components/Card';
 import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
 
@@ -7,6 +8,7 @@ function App() {
     <>
      <Navbar />
      <Hero />
+     <Card />
     </>
   );
 }
