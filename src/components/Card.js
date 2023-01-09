f7f41b695aa23b83e845952f2d7908088f229b3c
AbrahaKahsay <abrahak81@gmail.com>
2023-01-09 23:08:45 +0300

    Modify Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 import Image1 from "../images/image1.png"
-import Star1 from "../images/star1.png"
+import Star1 from "../images/Star1.png"
 import Elipse from "../images/Ellipse6.png"
 
 export const Card = () => {
   return (
     <section>
-      <img src={Image1} alt="card-picture-1" />
+      <img src={Image1} alt="card-pic-1" />
+      <div>
+        <img src={Star1} alt="star"/>
+        <span>5.0</span>
+        <span>(6)</span>
+        <span><img src={Elipse} alt="elipse"/></span>
+        <span>USA</span>
+      </div>
     </section>
   )
 }
